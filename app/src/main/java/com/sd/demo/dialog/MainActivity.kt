e719d1df62e8af73cd493930ac7193ee19c54fd9
zjun <565061763@qq.com>
2021-06-04 09:01:04 +0800

    demo

--- app/src/main/java/com/sd/demo/dialog/MainActivity.kt
+++ app/src/main/java/com/sd/demo/dialog/MainActivity.kt
@@ -1,92 +1,71 @@
-package com.sd.demo.dialog;
+package com.sd.demo.dialog
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.sd.demo.dialog.databinding.ActivityMainBinding
+import com.sd.lib.dialog.animator.SlideTopBottomParentCreator
+import com.sd.lib.dialog.impl.FDialog
 
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val TAG = MainActivity::class.java.simpleName
 
-import com.sd.demo.dialog.databinding.ActivityMainBinding;
-import com.sd.lib.dialog.IDialog;
-import com.sd.lib.dialog.animator.SlideTopBottomParentCreator;
-import com.sd.lib.dialog.impl.FDialog;
+    private lateinit var _binding: ActivityMainBinding
 
-import org.jetbrains.annotations.Nullable;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final String TAG = MainActivity.class.getSimpleName();
-
-    private ActivityMainBinding _binding;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        _binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(_binding.getRoot());
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
     }
 
-    private void showSimpleDemo() {
-        final FDialog dialog = new FDialog(this) {
-            @Override
-            protected void onContentViewChanged(@Nullable View oldView, @Nullable View contentView) {
-                super.onContentViewChanged(oldView, contentView);
-                Log.i(TAG, "onContentViewChanged oldView:" + oldView + " contentView:" + contentView);
+    private fun showSimpleDemo() {
+        val dialog: FDialog = object : FDialog(this@MainActivity) {
+            override fun onContentViewChanged(oldView: View?, contentView: View?) {
+                super.onContentViewChanged(oldView, contentView)
+                Log.i(TAG, "onContentViewChanged oldView:${oldView} contentView:${contentView}")
             }
 
-            @Override
-            protected void onCreate(@Nullable Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                Log.i(TAG, "onCreate savedInstanceState:" + savedInstanceState);
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                Log.i(TAG, "onCreate savedInstanceState:${savedInstanceState}")
             }
 
-            @Override
-            protected void onStart() {
-                super.onStart();
-                Log.i(TAG, "onStart");
+            override fun onStart() {
+                super.onStart()
+                Log.i(TAG, "onStart")
             }
 
-            @Override
-            protected void onStop() {
-                super.onStop();
-                Log.i(TAG, "onStop");
+            override fun onStop() {
+                super.onStop()
+                Log.i(TAG, "onStop")
             }
-        };
-
+        }
         /**
          * 设置填充
          */
-        dialog.setPadding(0, 0, 0, 0);
-
+        dialog.setPadding(0, 0, 0, 0)
         /**
          * 设置调试模式，内部会输出日志，日志tag：IDialog
          */
-        dialog.setDebug(true);
+        dialog.isDebug = true
         /**
          * 设置窗口要显示的内容
          */
-        dialog.setContentView(new Button(this));
-
+        dialog.setContentView(Button(this))
         /**
          * 设置窗口关闭监听
          */
-        dialog.setOnDismissListener(new IDialog.OnDismissListener() {
-            @Override
-            public void onDismiss(IDialog dialog) {
-                Log.i(TAG, "onDismiss");
-            }
-        });
-
+        dialog.setOnDismissListener {
+            Log.i(TAG, "onDismiss")
+        }
         /**
          * 设置窗口显示监听
          */
-        dialog.setOnShowListener(new IDialog.OnShowListener() {
-            @Override
-            public void onShow(IDialog dialog) {
-                Log.i(TAG, "onShow");
-            }
-        });
-
+        dialog.setOnShowListener {
+            Log.i(TAG, "onShow")
+        }
         /**
          * 设置窗口内容view动画创建对象，此处设置为透明度变化，可以实现AnimatorCreator接口来实现自定义动画
          *
@@ -102,25 +81,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
          * Gravity.BOTTOM:                      SlideTopBottomParentCreator 向上滑入，向下滑出
          *
          */
-        dialog.setAnimatorCreator(new SlideTopBottomParentCreator());
-
+        dialog.animatorCreator = SlideTopBottomParentCreator()
         /**
          * 设置动画时长
          */
-        dialog.setAnimatorDuration(1000);
-
+        dialog.setAnimatorDuration(1000)
         /**
          * 显示窗口
          */
-        dialog.show();
+        dialog.show()
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v == _binding.btnSimple) {
-            showSimpleDemo();
-        } else if (v == _binding.btnTarget) {
-            new PositionDialog(this, v).show();
+    override fun onClick(v: View) {
+        when (v) {
+            _binding.btnSimple -> showSimpleDemo()
+            _binding.btnTarget -> PositionDialog(this, v).show()
         }
     }
 }
