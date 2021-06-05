f8b129a152559738a35a6b0628aff92203536e97
zjun <565061763@qq.com>
2021-06-05 21:35:51 +0800

    demo

--- app/src/main/java/com/sd/demo/dialog/PositionDialog.kt
+++ app/src/main/java/com/sd/demo/dialog/PositionDialog.kt
@@ -1,86 +1,74 @@
-package com.sd.demo.dialog;
+package com.sd.demo.dialog
 
-import android.app.Activity;
-import android.view.View;
+import android.app.Activity
+import android.view.View
+import com.sd.demo.dialog.databinding.DialogPositionBinding
+import com.sd.lib.dialog.ITargetDialog
+import com.sd.lib.dialog.impl.FDialog
 
-import com.sd.demo.dialog.databinding.DialogPositionBinding;
-import com.sd.lib.dialog.ITargetDialog;
-import com.sd.lib.dialog.impl.FDialog;
+class PositionDialog : FDialog, View.OnClickListener {
+    private val _binding: DialogPositionBinding
+    private val _targetView: View
 
-public class PositionDialog extends FDialog implements View.OnClickListener {
-    private final View mTargetView;
-    private final DialogPositionBinding _binding;
-
-    public PositionDialog(Activity activity, View targetView) {
-        super(activity);
-        mTargetView = targetView;
-
-        setPadding(0, 0, 0, 0);
-        setContentView(R.layout.dialog_position);
-        _binding = DialogPositionBinding.bind(getContentView());
-
-        _binding.btnLeftTop.setOnClickListener(this);
-        _binding.btnLeftCenter.setOnClickListener(this);
-        _binding.btnLeftBottom.setOnClickListener(this);
-
-        _binding.btnRightTop.setOnClickListener(this);
-        _binding.btnRightCenter.setOnClickListener(this);
-        _binding.btnRightBottom.setOnClickListener(this);
-
-        _binding.btnTopLeft.setOnClickListener(this);
-        _binding.btnTopCenter.setOnClickListener(this);
-        _binding.btnTopRight.setOnClickListener(this);
-
-        _binding.btnBottomLeft.setOnClickListener(this);
-        _binding.btnBottomCenter.setOnClickListener(this);
-        _binding.btnBottomRight.setOnClickListener(this);
+    constructor(activity: Activity, targetView: View) : super(activity) {
+        _targetView = targetView
+        setPadding(0, 0, 0, 0)
+        setContentView(R.layout.dialog_position)
+        _binding = DialogPositionBinding.bind(contentView!!)
+        _binding.btnLeftTop.setOnClickListener(this)
+        _binding.btnLeftCenter.setOnClickListener(this)
+        _binding.btnLeftBottom.setOnClickListener(this)
+        _binding.btnRightTop.setOnClickListener(this)
+        _binding.btnRightCenter.setOnClickListener(this)
+        _binding.btnRightBottom.setOnClickListener(this)
+        _binding.btnTopLeft.setOnClickListener(this)
+        _binding.btnTopCenter.setOnClickListener(this)
+        _binding.btnTopRight.setOnClickListener(this)
+        _binding.btnBottomLeft.setOnClickListener(this)
+        _binding.btnBottomCenter.setOnClickListener(this)
+        _binding.btnBottomRight.setOnClickListener(this)
     }
 
-    @Override
-    public void onClick(View v) {
-        dismiss();
-        final PopDialog popDialog = new PopDialog((Activity) getContext());
-
-        switch (v.getId()) {
-            case R.id.btn_left_top:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.LeftOutsideTop);
-                break;
-            case R.id.btn_left_center:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.LeftOutsideCenter);
-                break;
-            case R.id.btn_left_bottom:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.LeftOutsideBottom);
-                break;
-
-            case R.id.btn_right_top:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.RightOutsideTop);
-                break;
-            case R.id.btn_right_center:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.RightOutsideCenter);
-                break;
-            case R.id.btn_right_bottom:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.RightOutsideBottom);
-                break;
-
-            case R.id.btn_top_left:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.TopOutsideLeft);
-                break;
-            case R.id.btn_top_center:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.TopOutsideCenter);
-                break;
-            case R.id.btn_top_right:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.TopOutsideRight);
-                break;
-
-            case R.id.btn_bottom_left:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.BottomOutsideLeft);
-                break;
-            case R.id.btn_bottom_center:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.BottomOutsideCenter);
-                break;
-            case R.id.btn_bottom_right:
-                popDialog.target().show(mTargetView, ITargetDialog.Position.BottomOutsideRight);
-                break;
+    override fun onClick(v: View) {
+        dismiss()
+        val popDialog = PopDialog((context as Activity))
+        when (v) {
+            _binding.btnLeftTop -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.LeftOutsideTop)
+            }
+            _binding.btnLeftCenter -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.LeftOutsideCenter)
+            }
+            _binding.btnLeftBottom -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.LeftOutsideBottom)
+            }
+            _binding.btnRightTop -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.RightOutsideTop)
+            }
+            _binding.btnRightCenter -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.RightOutsideCenter)
+            }
+            _binding.btnRightBottom -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.RightOutsideBottom)
+            }
+            _binding.btnTopLeft -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.TopOutsideLeft)
+            }
+            _binding.btnTopCenter -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.TopOutsideCenter)
+            }
+            _binding.btnTopRight -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.TopOutsideRight)
+            }
+            _binding.btnBottomLeft -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.BottomOutsideLeft)
+            }
+            _binding.btnBottomCenter -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.BottomOutsideCenter)
+            }
+            _binding.btnBottomRight -> {
+                popDialog.target().show(_targetView, ITargetDialog.Position.BottomOutsideRight)
+            }
         }
     }
 }
