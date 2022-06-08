1b9828a183578643bf42948f758db977808e9db4
zhengjun <565061763@qq.com>
2022-06-09 01:22:51 +0800

    format

--- lib/src/main/java/com/sd/lib/dialog/animator/PivotPercentCreator.java
+++ lib/src/main/java/com/sd/lib/dialog/animator/PivotPercentCreator.java
@@ -9,29 +9,29 @@ import android.view.View;
 public class PivotPercentCreator extends BaseAnimatorCreator {
     private final float mPivotPercentX;
     private final float mPivotPercentY;
-
     private final PivotCreator mPivotCreator;
 
     /**
-     * @param creator
      * @param pivotPercentX x方向锚点百分比[0-1]
      * @param pivotPercentY y方向锚点百分比[0-1]
      */
     public PivotPercentCreator(AnimatorCreator creator, float pivotPercentX, float pivotPercentY) {
         mPivotPercentX = pivotPercentX;
         mPivotPercentY = pivotPercentY;
-
-        mPivotCreator = new PivotCreator(creator, new PivotCreator.PivotProvider() {
-            @Override
-            public float getPivot(boolean show, View view) {
-                return mPivotPercentX * view.getWidth();
-            }
-        }, new PivotCreator.PivotProvider() {
-            @Override
-            public float getPivot(boolean show, View view) {
-                return mPivotPercentY * view.getHeight();
-            }
-        });
+        mPivotCreator = new PivotCreator(creator,
+                new PivotCreator.PivotProvider() {
+                    @Override
+                    public float getPivot(boolean show, View view) {
+                        return mPivotPercentX * view.getWidth();
+                    }
+                },
+                new PivotCreator.PivotProvider() {
+                    @Override
+                    public float getPivot(boolean show, View view) {
+                        return mPivotPercentY * view.getHeight();
+                    }
+                }
+        );
     }
 
     @Override
