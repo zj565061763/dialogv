4b09469c8ae7d041de5fccce0e63ce2be2323fd4
565061763 <565061763@qq.com>
2022-11-25 19:22:27 +0800

    FDialogHolder

--- lib/src/main/java/com/sd/lib/dialog/impl/FDialog.kt
+++ lib/src/main/java/com/sd/lib/dialog/impl/FDialog.kt
@@ -878,3 +878,51 @@ private fun isViewUnder(view: View?, x: Int, y: Int): Boolean {
         x >= view.left && x < view.right && y >= view.top && y < view.bottom
     }
 }
+
+
+private object FDialogHolder {
+    private val dialogHolder: MutableMap<Activity, MutableList<FDialog>> = hashMapOf()
+
+    @JvmStatic
+    fun addDialog(dialog: FDialog) {
+        val activity = dialog.ownerActivity
+        val holder = dialogHolder[activity] ?: mutableListOf<FDialog>().also {
+            dialogHolder[activity] = it
+        }
+
+        holder.lastOrNull()?.notifyCover()
+        holder.add(dialog)
+    }
+
+    @JvmStatic
+    fun removeDialog(dialog: FDialog) {
+        val activity = dialog.ownerActivity
+        val holder = dialogHolder[activity] ?: return
+
+        val remove = holder.remove(dialog)
+        if (remove) {
+            holder.lastOrNull()?.notifyCoverRemove()
+        }
+
+        if (holder.isEmpty()) {
+            dialogHolder.remove(activity)
+        }
+    }
+
+    @JvmStatic
+    fun get(activity: Activity): List<FDialog>? {
+        val holder = dialogHolder[activity] ?: return null
+        return holder.toMutableList()
+    }
+
+    @JvmStatic
+    fun getLast(activity: Activity): FDialog? {
+        val holder = dialogHolder[activity] ?: return null
+        return holder.lastOrNull()
+    }
+
+    @JvmStatic
+    fun remove(activity: Activity) {
+        dialogHolder.remove(activity)
+    }
+}
