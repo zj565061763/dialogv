c35e2cc8a4ca77eea1182e53b9b774fbc7dba182
565061763 <565061763@qq.com>
2022-11-25 18:07:59 +0800

    check state before request focus

--- lib/src/main/java/com/sd/lib/dialog/impl/FDialog.kt
+++ lib/src/main/java/com/sd/lib/dialog/impl/FDialog.kt
@@ -617,7 +617,7 @@ open class FDialog(activity: Activity) : IDialog {
         private val _checkFocusRunnable = object : Runnable {
             override fun run() {
                 if (!this@InternalDialogView.isAttachedToWindow) return
-                if (FDialogHolder.getLast(_activity) == this@FDialog) {
+                if (_state.isShowPart && FDialogHolder.getLast(_activity) == this@FDialog) {
                     if (!hasFocus()) {
                         if (isDebug) {
                             Log.i(IDialog::class.java.simpleName, "requestFocus ${this@FDialog}")
