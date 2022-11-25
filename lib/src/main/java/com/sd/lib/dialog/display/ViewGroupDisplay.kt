cf622ef0e4ee31150f172afa07fd226d2b95af16
565061763 <565061763@qq.com>
2022-11-25 13:56:11 +0800

    simplify

--- lib/src/main/java/com/sd/lib/dialog/display/ViewGroupDisplay.kt
+++ lib/src/main/java/com/sd/lib/dialog/display/ViewGroupDisplay.kt
@@ -4,12 +4,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.sd.lib.dialog.IDialog
 
-class ViewGroupDisplay : IDialog.Display {
-    private val _viewGroup: ViewGroup
-
-    constructor(viewGroup: ViewGroup) {
-        _viewGroup = viewGroup
-    }
+class ViewGroupDisplay(viewGroup: ViewGroup) : IDialog.Display {
+    private val _viewGroup = viewGroup
 
     override fun showDialog(view: View) {
         _viewGroup.addView(
